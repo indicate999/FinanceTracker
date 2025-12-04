8f466c61a36b4bf0423e346e4c8456f6cce98290
indicate999 <inovitskiy13@gmail.com>
2025-12-04 20:04:18 +0100

    Fix line ending

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 # Substitute environment variables in nginx config
 envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-# Start nginx
+# Start nginx 
 exec nginx -g 'daemon off;'
